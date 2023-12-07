b1d3b22322fa1036b3edf5fe34e23a2128bebe9f
傅暄桓 <t13762@ms.sltung.com.tw>
2023-12-07 13:13:46 +0800

    update: extract process into handlers update: scan css add: openai api field filling input

--- ai-json-form/src/axios.js
+++ ai-json-form/src/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const Request = axios.create({
   baseURL: `${import.meta.env.VITE_APP_BASE_URL}/api`,
+  timeout: 1000,
 });
 
 export const getVersion = () => Request.get("/version");
