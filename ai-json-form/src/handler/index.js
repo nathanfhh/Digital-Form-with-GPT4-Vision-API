891df75679c4dc42321be9321ff5b7ac3d9389c5
傅暄桓 <t13762@ms.sltung.com.tw>
2023-12-07 13:11:23 +0800

    add: request to openai api in frontend logic

---
+++ ai-json-form/src/handler/index.js
@@ -0,0 +1,7 @@
+import { SocketIOBackendHandler } from './socketHandler.js'
+import { FrontendOnlyHandler } from './frontendHandler.js'
+
+export const handlers = {
+  SocketIOBackendHandler,
+  FrontendOnlyHandler
+}
