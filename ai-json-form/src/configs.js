c74e03ddf3c1c7ec553e226e967f1a73a91b90d5
傅暄桓 <t13762@ms.sltung.com.tw>
2025-05-17 16:55:35 +0800

    refactor: enhance AI response handling and update PDF processing logic

--- ai-json-form/src/configs.js
+++ ai-json-form/src/configs.js
@@ -1,58 +1,54 @@
 export const basicSchema = {
-  title: '測試用表單',
-  pages: [
-    {
-      name: 'page1',
-      elements: [
+    "title": "測試用表單",
+    "pages": [
         {
-          type: 'text',
-          name: 'firstName',
-          title: 'First name',
-          defaultValue: 'Jun',
-          isRequired: true
-        },
-        {
-          type: 'text',
-          name: 'lastName',
-          title: 'Last name',
-          description: '請輸入您的姓氏',
-          isRequired: true
-        },
-        {
-          type: 'text',
-          name: 'price',
-          title: '價格',
-          inputType: 'number'
-        },
-        {
-          type: 'text',
-          name: 'age',
-          title: '年紀',
-          inputType: 'number',
-          min: 16,
-          max: 80
-        },
-        {
-          type: 'comment',
-          name: 'bio',
-          title: '自我介紹',
-          minLength: 10
-        },
-        {
-          type: 'text',
-          name: 'password',
-          title: 'Password',
-          inputType: 'password',
-          minLength: 3
-        },
-        {
-          type: 'text',
-          name: 'telephone',
-          title: 'Telephone',
-          inputType: 'tel',
-          minLength: 10
+            "name": "page1",
+            "elements": [
+                {
+                    "type": "text",
+                    "name": "firstName",
+                    "title": "First name",
+                    "isRequired": true
+                },
+                {
+                    "type": "text",
+                    "name": "lastName",
+                    "title": "Last name",
+                    "description": "請輸入您的姓氏",
+                    "isRequired": true
+                },
+                {
+                    "type": "text",
+                    "name": "price",
+                    "title": "價格",
+                    "inputType": "number"
+                },
+                {
+                    "type": "text",
+                    "name": "age",
+                    "title": "年紀",
+                    "inputType": "number",
+                    "min": 16,
+                    "max": 80
+                },
+                {
+                    "type": "comment",
+                    "name": "bio",
+                    "title": "自我介紹"
+                },
+                {
+                    "type": "text",
+                    "name": "password",
+                    "title": "Password",
+                    "inputType": "password"
+                },
+                {
+                    "type": "text",
+                    "name": "telephone",
+                    "title": "Telephone",
+                    "inputType": "tel"
+                }
+            ]
         }
-      ]
-    }
-  ]
+    ]
 }
